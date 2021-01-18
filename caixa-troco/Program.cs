7857fc5e0295ca7d5939441109a88da53787782f
Andressa Bovolenta <andressa.bbovolenta@gmail.com>
2021-01-18 17:52:19 -0300

    First commit

---
+++ caixa-troco/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace caixa_troco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CAXA - TROCO
+
+            Console.WriteLine("Digite o valor do produto:");
+            double preco = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Insira o valor pago pelo cliente:");
+            double recebido = Convert.ToDouble(Console.ReadLine());
+            double troco = recebido - preco;
+            if (troco != 0)
+            {
+                Console.WriteLine("Seu troco é de R$" + troco + ". Obrigado(a) pela preferência! Volte sempre!");
+            }
+            else
+            {
+                Console.WriteLine("Obrigado(a) pela preferência! Volte sempre!");
+            }
+
+            //DÚVIDA: Como criar o loop para que o console continue a exibir a primeira instrução enquanto o usuário não apertar "enter" (vazio)?
+        }
+    }
+}
